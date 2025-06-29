50975d73d603bd22326ddd11e5f99e0c3639da14
Ayesha-Jan <ayeshajan667@gmail.com>
2025-06-29 12:27:56 +0200

    Adding grid for designer

--- lib/pages/designer.dart
+++ lib/pages/designer.dart
@@ -91,9 +91,110 @@ class _DesignerState extends State<Designer> {
                     );
                   }).toList(),
                 ),
-
+                DropdownButton<int>(
+                  value: gridSize,
+                  onChanged: (int? value) {
+                    setState(() {
+                      gridSize = value!;
+                      _initializeGrid();
+                    });
+                  },
+                  items: [8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30].map((size) {
+                    return DropdownMenuItem(value: size, child: Text('$size x $size'));
+                  }).toList(),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      gridGenerated = true;
+                      _initializeGrid();
+                    });
+                  },
+                  child: Text("Generate Grid"),
+                ),
               ],
-            )
+            ),
+            SizedBox(height: 16),
+            if(gridGenerated) ...[
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Select ${selectedMode == Mode.colour ? "Color" : "Stitch"}:",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+              ),
+
+              SizedBox(height: 8),
+
+              selectedMode == Mode.colour
+              ? Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  for (String stitch in ["sc", "dc", "tr"])
+                    GestureDetector(
+                      onTap: () => setState(() => selectedTool = stitch),
+                      child: Container(
+                        width: 50,
+                        height: 50,
+                        margin: EdgeInsets.symmetric(horizontal: 6),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: selectedTool == stitch ? Colors.black : Colors.grey,
+                            width: 2,
+                          ),
+                        ),
+                        child: Center(
+                          child: Image.asset(
+                            'assets/stitches/$stitch.png',
+                            height: 30,
+                          ),
+                        ),
+                      )
+                    ),
+                ],
+              ):
+
+              SizedBox(height: 16),
+
+              Expanded(
+                child: GridView.builder(
+                  itemCount: gridSize * gridSize,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: gridSize,
+                  ),
+                  itemBuilder: (context, index) {
+                    int row = index ~/ gridSize;
+                    int col = index % gridSize;
+                    var cell = gridData[row][col];
+
+                    return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          gridData[row][col] = selectedTool;
+                        });
+                      },
+                      child: Container(
+                        margin: EdgeInsets.all(1),
+                        decoration: BoxDecoration(
+                          color: selectedMode == Mode.colour
+                            ? (cell ?? Colors.white)
+                            : Colors.white,
+                          border: Border.all(color: Colors.black12),
+                        ),
+                        child: selectedMode != Mode.colour && cell != null
+                          ? Center(
+                            child: Image.asset(
+                              'assets/stitches/$cell.png',
+                              height: 30,
+                            ),
+                          )
+                        : null,
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
           ],
         ),
       ),
