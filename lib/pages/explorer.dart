67e84ba8e96e2f345c9737ec548d952fd19fc053
Ayesha-Jan <ayeshajan667@gmail.com>
2025-07-02 12:01:00 +0200

    Creating layout for explorer page

--- lib/pages/explorer.dart
+++ lib/pages/explorer.dart
@@ -7,9 +7,56 @@ class Explorer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Explorer"),
+        title: Text(
+          "E X P L O R E R",
+          style: TextStyle(
+            fontSize: 28,
+            color: Color(0xFFEA467E),
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         centerTitle: true,
-        backgroundColor: Colors.pinkAccent[100],
+        backgroundColor: Color(0xFFDCE7FB),
+      ),
+      drawer: Drawer(
+        backgroundColor: Color(0xFFDCE7FB),
+        child: Column(
+          children: [
+            DrawerHeader(
+                child: Icon(
+                  Icons.favorite,
+                  size: 48,
+                )
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text("H O M E"),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, '/home');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.design_services),
+              title: Text("D E S I G N E R"),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, '/designer');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.explore),
+              title: Text("E X P L O R E R"),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushNamed(context, '/explorer');
+              },
+            ),
+          ],
+        ),
+      ),
+      body: Container(
+        color: Color(0xFFFDDBE6),
       ),
     );
   }
